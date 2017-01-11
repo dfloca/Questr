13ea3a3a161e95d5e0454d462a338552397924c6
Cristian Cioanca <cristian.cioanca@hotmail.com>
2017-01-10 21:32:32 -0500

    itty bitty change.
    if you try to put a timeframe in the input of the TOD input field, it
    will give you a warning.

--- RPGManager/RPGManager/Scripts/questr.dmpage.js
+++ RPGManager/RPGManager/Scripts/questr.dmpage.js
@@ -237,7 +237,25 @@
         //check to see if input has a ":". if so, they may be trying to add a set timeframe, do that.
         else if(input.includes(":"))
         {
-            var ar = input.split(":");
+            //var ar = input.split(":");
+            toastr.options = {
+                "closeButton": true,
+                "debug": false,
+                "newestOnTop": true,
+                "progressBar": false,
+                "positionClass": "toast-bottom-left",
+                "preventDuplicates": false,
+                "onclick": null,
+                "showDuration": "300",
+                "hideDuration": "1000",
+                "timeOut": "5000",
+                "extendedTimeOut": "1000",
+                "showEasing": "swing",
+                "hideEasing": "linear",
+                "showMethod": "fadeIn",
+                "hideMethod": "fadeOut"
+            }
+            Command: toastr["warning"]("We don't allow that yet, though we will soon.", "Warning");
         }
     });
 });
